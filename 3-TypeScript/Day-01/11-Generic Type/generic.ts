24070d6ed53845147dda4abe5320139bd2ed13f9
Smey09 <roemreaksmey7@gmail.com>
2024-07-22 12:57:52 +0700

    testing

--- 3-TypeScript/Day-01/11-Generic Type/generic.ts
+++ 3-TypeScript/Day-01/11-Generic Type/generic.ts
@@ -7,3 +7,5 @@ let output2 = identity<number>(123);
 
 console.log("Output 1:", output1); // type of output1 is string
 console.log("Output 2:", output2); // type of output2 is number
+
+//heloo Kali
